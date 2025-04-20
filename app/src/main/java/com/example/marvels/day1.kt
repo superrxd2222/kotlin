67199f95aa3ea43e1ed5c75ce6dfc8226d594a69
superxd2222 <sumisonxd@gmail.com>
2025-04-20 10:35:27 +0545

    Initial commit

---
+++ app/src/main/java/com/example/marvels/day1.kt
@@ -0,0 +1,23 @@
+package com.example.marvels
+
+fun main() {
+    println("Hello World")
+    val age = 10
+
+
+//    var name = "a"
+//    name="b"
+
+    var name : String = "c"
+    println("Hello i am $name and i am $age yeards old")
+
+
+//    var age = arrayOf(10,20,30)
+//    age[3] = 50
+    var male = arrayListOf<String>("A","B")
+    var female = ArrayList<Any>()
+
+    female.add("d")
+
+
+}
